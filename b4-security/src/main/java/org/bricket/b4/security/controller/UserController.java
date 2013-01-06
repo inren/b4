c1aabe64bcf29e54438162876bda400e9113b38f
Henning Teek <henning.teek@bricket.org>
2013-01-06 01:37:10 +0100

    restructuring with new maven modules and package names

---
+++ b4-security/src/main/java/org/bricket/b4/security/controller/UserController.java
@@ -0,0 +1,49 @@
+/**
+ * Copyright 2011 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
+ * implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.bricket.b4.security.controller;
+
+import org.bricket.b4.core.controller.ResourceNotFoundException;
+import org.bricket.b4.security.entity.User;
+import org.bricket.b4.security.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+	@Autowired
+	private UserRepository userRepository;
+
+	@RequestMapping(method = RequestMethod.GET)
+	@ResponseBody
+	public Iterable<User> getUsers() {
+		return userRepository.findAll();
+	}
+
+	@RequestMapping(value = "/{userId}", method = RequestMethod.GET)
+	@ResponseBody
+	public User getUser(@PathVariable("userId") User user) {
+		if (user == null) {
+			throw new ResourceNotFoundException();
+		}
+		return user;
+	}
+}
